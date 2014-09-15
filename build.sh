f22f394a8f1f95e4769046cd13c253e83a05e6a1
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-09-15 23:18:40 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
 npm install
+bower install
 grunt
 
 app/console assets:install web --symlink --env=prod
