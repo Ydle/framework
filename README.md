e2781acb519de0a12ab122dc424c2a4f625593c5
Mbechezi Mlanawo <mlanawo.mbechezi@ikimea.com>
2014-09-15 15:43:33 +0200

    cleanup

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ YdleFMK
 =======
 
 Ydle distribution based on Symfony 2
+
+
+## Build assets
+
+
+### Install NodeJS
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+
+```s
+npm install
+grunt
+```
+
+
--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-bin/bowerphp update
+
+wget http://node-arm.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+npm install
+grunt
+
 app/console assets:install web --symlink --env=prod
 app/console assetic:dump --env=prod
 app/console cache:clear --env=prod
