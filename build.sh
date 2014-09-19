aeec3550cd90ab312a65caa19640ccae7cf144f0
Yaug <manuel.esteban@chroniques-lunaires.com>
2014-09-19 15:01:21 +0200

    Final modification for grunt use

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+bower install
 app/console assets:install web --symlink --env=prod
+grunt
 app/console assetic:dump --env=prod
 app/console cache:clear --env=prod
 chmod -R 777 app/cache
