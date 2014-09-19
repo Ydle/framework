593701f635a7aa0424e8a5723b316587713c80c2
Yaug <manuel.esteban@chroniques-lunaires.com>
2014-09-19 09:07:44 +0200

    Remove bugging npm install

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 
 wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 sudo dpkg -i node_latest_armhf.deb
-npm install
-bower install
-grunt
 
 app/console assets:install web --symlink --env=prod
 app/console assetic:dump --env=prod
